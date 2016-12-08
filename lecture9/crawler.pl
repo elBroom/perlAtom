0e22d75b39ef372cb3bfb115a8dd0fdffb1bf2bb
elBroom <Zarina1458@ya.com>
2016-12-09 00:49:38 +0300

    hw9 fix redirect

--- lecture9/crawler.pl
+++ lecture9/crawler.pl
@@ -27,25 +27,29 @@ sub get_length_page{
 		$urls{$url} = 0;
 		$cv->begin;
 		http_get $site.$url,
+		recurse => 0,
 		on_header => sub {
 			my ($header) = @_;
 			return 0 unless($header->{"content-type"} =~ m{^text/html\s*(?:;|$)});
-			return 1 unless($header->{"Redirect"});
-			return 0 unless($header->{"URL"} =~ m{^$site});
-			$url = $_[0]{"URL"} =~ s/$site//r;
-			return !defined $urls{$url};
+			if($header->{"Status"} =~ m{^30\d$}){
+				if($header->{"location"} =~ m{^$site}){
+					# $url = $header->{"location"} =~ s/$site//r;
+					get_length_page($url);
+				}
+				return 0;
+			}
+			return 1;
 		},
 		sub {
 			my ($body, $header) = @_;
 			if($header->{'Status'} == 200){
-				$urls{$url} = $header->{'content-length'};
-				$sum += $header->{'content-length'};
-				my $es_site = quotemeta($site);
+				$urls{$url} = length $body;
+				$sum += $urls{$url};
 
 				my $tree = HTML::TreeBuilder::XPath->new_from_content($body);
 				for ($tree->findnodes('//a[@href]')){
 					if($_ && $_->attr('href') =~
-						m{^(?:$es_site)?(\/[\w\/\-_\(\)]*)(?:\?[^"]+)?$}ig){
+						m{^(?:$site)?(/[\w/\-_\(\)]*)(?:\?[^"]+)?$}ig){
 						# say Dumper($1);
 						last if(is_limit);
 						get_length_page($1);
@@ -61,7 +65,7 @@ get_length_page("/");
 $cv->recv;
 # say Dumper(scalar (keys %urls));
 
-say "Top-10";
 $count_top = $count_top < keys %urls ? $count_top : keys %urls;
+say "Top-$count_top";
 say $site.$_ for ((sort {$urls{$b} <=> $urls{$a}} keys %urls)[0..$count_top-1]);
 say "\ntotal: $sum";
